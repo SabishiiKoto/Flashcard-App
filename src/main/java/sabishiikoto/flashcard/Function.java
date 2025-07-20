57d7af328e8e40b32ff7c2fae036885d51a94798
Anh Nguyen <anh.nguyen.huymai@gmail.com>
2025-07-20 15:32:57 -0600

    Deleted unused methods.

--- src/main/java/sabishiikoto/flashcard/Function.java
+++ src/main/java/sabishiikoto/flashcard/Function.java
@@ -3,23 +3,23 @@ package sabishiikoto.flashcard;
 import java.util.*;
 
 public class Function {
-    public static ArrayList<Category> removeCategory(ArrayList<Category> categories, Category category){
-        for (Category item : categories){
-            if (item.getName().equals(category.getName())){
-                categories.remove(category);
-            }
-        }
-        return categories;
-    }
-    public static ArrayList<Category> addCategory(ArrayList<Category> categories, Category category){
-        for (Category item : categories){
-            if (item.getName().equals(category.getName())){
-                return categories;
-            }
-        }
-        categories.add(category);
-        return categories;
-    }
+//    public static ArrayList<Category> removeCategory(ArrayList<Category> categories, Category category){
+//        for (Category item : categories){
+//            if (item.getName().equals(category.getName())){
+//                categories.remove(category);
+//            }
+//        }
+//        return categories;
+//    }
+//    public static ArrayList<Category> addCategory(ArrayList<Category> categories, Category category){
+//        for (Category item : categories){
+//            if (item.getName().equals(category.getName())){
+//                return categories;
+//            }
+//        }
+//        categories.add(category);
+//        return categories;
+//    }
     public static ArrayList<String> termsToList (Category category){
         ArrayList<String> termStringList = new ArrayList<>();
         for (Term term : category.getTerms()){
